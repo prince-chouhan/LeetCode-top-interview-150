556c8fa9cab0ddefc372fed176aa1a7f8a4b01b1
Prince Chouhan <princechouhan1708@gmail.com>
2026-03-30 23:14:40 +0530

    leetCode 27

---
+++ Remove Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] != val) {
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            j++;
+            }
+        }
+        return j;
+    }
+}
